3e774719e112ceb480dfeece93b45d99fa3b7f32
emuel-vassallo <emuelvassallo15@gmail.com>
2022-04-28 23:27:56 +0200

    Added showOutcome function

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function computerPlay() {
   let randomIndex = getRandomIndex()
   return hands[randomIndex]
 }
+
+function showOutcome(playerSelection, computerSelection) {
+  let playerSelection = playerSelection.toLowerCase()
+  let computerSelection = computerSelection.toLowerCase()
+
+  if (playerSelection === computerSelection) {
+    return "It's a Tie"
+  }
+}

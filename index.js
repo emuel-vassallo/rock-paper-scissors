ab6dce1cd6501e5b958fd4f25e2c7fec68d6d373
emuel-vassallo <emuelvassallo15@gmail.com>
2022-04-28 23:32:20 +0200

    Removed 'let' to fix redeclaration error

--- index.js
+++ index.js
@@ -8,11 +8,37 @@ function computerPlay() {
   return hands[randomIndex]
 }
 
+// FUNCTION showOutcome(playerSelection, computerSelection):
+//   if both the player and the computer select the same,
+//   the outcome is 'DRAW'.
+//
+//   else, if player chooses Rock and the computer chooses Paper,
+//   the outcome is 'You Lose! Paper beats Rock'
+//
+//   else, if player chooses Rock and the computer chooses Scissors,
+//   the outcome is 'You Win! Rock beats Scissors'
+//
+//   else, if player chooses Paper and the computer chooses Rock,
+//   the outcome is 'You Win! Paper beats Rock'
+//
+//   else, if player chooses Paper and the computer chooses Scissors,
+//   the outcome is 'You Lose! Scissors beat Paper'
+//
+//   else, if player chooses Scissors and the computer chooses Rock,
+//   the outcome is 'You Lose! Rock beats Scissors'
+//
+//   else, if player chooses Scissors and the computer chooses Paper,
+//   the outcome is 'You Win! Scissors beats Paper'
+//
+//   return the outcome
+
 function showOutcome(playerSelection, computerSelection) {
-  let playerSelection = playerSelection.toLowerCase()
-  let computerSelection = computerSelection.toLowerCase()
+  playerSelection = playerSelection.toLowerCase()
+  computerSelection = computerSelection.toLowerCase()
 
   if (playerSelection === computerSelection) {
     return "It's a Tie"
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return 'You Lose! Paper beats Rock'
   }
 }
